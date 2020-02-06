fd2be4136e09a03b654f64aa6a9f3a23120c5d2f
Dominic Kudiabor <dominickud@Dominics-MacBook-Pro.local>
2020-02-06 15:11:06 +0200

    Updates

---
+++ precache-manifest.65efe0631dcc3ce29cd091bc4fac2958.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26e43ffffc0172376dbff7cf5ca6270d",
+    "url": "/,io/monster/index.html"
+  },
+  {
+    "revision": "043e2369498217fc3505",
+    "url": "/,io/monster/static/css/main.cc5e1949.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/,io/monster/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/,io/monster/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "043e2369498217fc3505",
+    "url": "/,io/monster/static/js/main.6a4e5b21.chunk.js"
+  },
+  {
+    "revision": "faab52430e5ccd283946",
+    "url": "/,io/monster/static/js/runtime-main.351b968c.js"
+  }
+]);
